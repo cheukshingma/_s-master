061c3ba39348919ff10659d80acdc25c6d448a1d
cheukshingma <cheukshing.ma@utoronto.ca>
2015-08-07 13:55:25 -0400

    footer
    copyright

--- footer.php
+++ footer.php
@@ -15,7 +15,9 @@
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), 'Terra Motive', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?> 
+			<!-- Footer Copyright --> 
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
