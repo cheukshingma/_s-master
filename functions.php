90644909116c32c26c0d2d4b141f566192642a29
cheukshingma <cheukshing.ma@utoronto.ca>
2015-08-07 12:42:28 -0400

    Enqueue scripts and styles
    Enqueueing scripts and styles

--- functions.php
+++ functions.php
@@ -115,7 +115,11 @@ add_action( 'widgets_init', '_s_widgets_init' );
  */
 function _s_scripts() {
 	wp_enqueue_style( '_s-style', get_stylesheet_uri() );
-
+	
+	wp_enqueue_style('terra-motive-content-sidebar', get_template_directory_uri() . '/layouts/content-sidebar.css'	); /* Layouts - Content-Sidebars.com */
+	
+	wp_enqueue_style('terra-motive-fontawesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css'); /* Font Custom Icons */ 
+	
 	wp_enqueue_script( '_s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
 	wp_enqueue_script( '_s-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
