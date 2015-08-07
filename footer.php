694b5ea63d33750caed6eee3e5efb81ca52a563f
cheukshingma <cheukshing.ma@utoronto.ca>
2015-08-07 16:05:48 -0400

    editing footer
    editing footer

--- footer.php
+++ footer.php
@@ -2,9 +2,11 @@
 /**
  * The template for displaying the footer.
  *
- * Contains the closing of the #content div and all content after
+ * Contains the closing of the #content div and all content after.
  *
- * @package _s
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package terra motive
  */
 
 ?>
@@ -13,11 +15,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'terra-motive' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'terra-motive' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), 'Terra Motive', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?> 
-			<!-- Footer Copyright --> 
-			
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'terra-motive' ), 'Terra Motive', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
