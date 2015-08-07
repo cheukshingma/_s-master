e91bd0e0bb69d868ca9854ee396b3544bbc5c5ed
cheukshingma <cheukshing.ma@utoronto.ca>
2015-08-07 13:30:25 -0400

    adding php to control article
    controlling article and adding arrow to read more link

--- template-parts/content.php
+++ template-parts/content.php
@@ -20,12 +20,16 @@
 
 	<div class="entry-content">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', '_s' ), array( 'span' => array( 'class' => array() ) ) ),
+			the_post_thumbnail('medium');
+			/* Translators: %s: Name of current post */
+			the_excerpt( sprintf(
+				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'terra-motive' ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
-		?>
+		/* [Read More -->] Arrow */
+			
+		?><span class="read_more"><a href="<?php the_permalink(); ?>">[ Read More &#8611; ]</a></span>
+
 
 		<?php
 			wp_link_pages( array(
