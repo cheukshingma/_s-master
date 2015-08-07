4835b0160ab78ae182ce5a02359c73f294a3448e
cheukshingma <cheukshing.ma@utoronto.ca>
2015-08-07 15:10:43 -0400

    editing content.php
    editing content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -2,7 +2,9 @@
 /**
  * Template part for displaying posts.
  *
- * @package _s
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package terra motive
  */
 
 ?>
@@ -13,33 +15,34 @@
 
 		<?php if ( 'post' == get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php _s_posted_on(); ?>
+			<?php terra_motive_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
-			the_post_thumbnail('medium');
-			/* Translators: %s: Name of current post */
+			the_post_thumbnail("large");
 			the_excerpt( sprintf(
-				__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'terra-motive' ),
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'terra-motive' ), array( 'span' => array( 'class' => array() ) ) ),
 				the_title( '<span class="screen-reader-text">"', '"</span>', false )
 			) );
+		
 		/* [Read More -->] Arrow */
 			
 		?><span class="read_more"><a href="<?php the_permalink(); ?>">[ Read More &#8611; ]</a></span>
 
-
 		<?php
 			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', '_s' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'terra-motive' ),
 				'after'  => '</div>',
 			) );
 		?>
 	</div><!-- .entry-content -->
+		
 
 	<footer class="entry-footer">
-		<?php _s_entry_footer(); ?>
+		<?php terra_motive_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
